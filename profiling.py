94d6e0b17da854610354261b4d88c70cc9c52cfc
20171609 <clw3wb@naver.com>
2023-12-30 23:37:42 +0900

    fix : profiling bug

--- profiling.py
+++ profiling.py
@@ -163,9 +163,9 @@ def b_profiling(data_path, t, parameter, min_data, dataset_path):
                         profile, profile_key = profiling(flow_stack[target_ip]['flow'], target_ip, flow_stack[target_ip]['st_time'], flow_stack[target_ip]['end_time'])
                         
                         tmp = []
-                        for profile in profile_list:
-                            for i, feature in enumerate(feature_list):
-                                tmp.append(feature_func_map[feature](profile))
+
+                        for i, feature in enumerate(feature_list):
+                            tmp.append(feature_func_map[feature](profile))
                         
                         feature.append(tmp)
                         profile_key_list.append(f"{check_label(flow_stack[target_ip]['label'])}+{profile_key}+{file_name}")
