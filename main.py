6873ef2fe170f591e393388949810db3b0ad0c56
20171609 <clw3wb@naver.com>
2024-01-11 17:06:08 +0900

    Debug : chagne file name

--- main.py
+++ main.py
@@ -12,9 +12,7 @@ from utils import *
 from profiling import b_profiling
 import traceback
 
-
-
-def main(dataset_path, attack, change_feature, seperate, change_src, test_method, confidence, separate_attackIP):
+def main(dataset_path, attack, change_feature, seperate, change_src, test_method, confidence, separate_attackIP, count_prot):
     # dataset_path = "CTU-Rbot"\
 
     # Profiling에 사용
@@ -35,7 +33,7 @@ def main(dataset_path, attack, change_feature, seperate, change_src, test_method
     train_path = [rf"dataset\{dataset_path}\train\{file}" for file in os.listdir(os.path.join("./dataset", dataset_path, 'train'))]
     test_path = [rf"dataset\{dataset_path}\test\{file}" for file in os.listdir(os.path.join("./dataset", dataset_path, 'test'))]
 
-    global_.initialize(train_path[0], change_src, change_feature, seperate, attack, test_method,separate_attackIP)
+    global_.initialize(train_path[0], change_src, change_feature, seperate, attack, test_method,separate_attackIP, count_prot)
 
     parameter = f"cs({change_src})_cf({change_feature})_sepIP({separate_attackIP})_min({min_data})"
 
@@ -140,6 +138,30 @@ def main(dataset_path, attack, change_feature, seperate, change_src, test_method
 
         test_data = [f"{test}{src}" for test, src in zip(test_data, test_src)]
 
+    if count_prot:
+        #데이터 불러오기
+        folder = f'./preprocessing/{dataset_path}/profiling/{parameter}'
+
+        train_port = []
+        test_port = []
+        
+        # 'train_feature'으로 시작하는 모든 파일 찾기
+        train_ffiles_prt = glob.glob(os.path.join(folder, 'train_protflag_*'))
+        train_ffiles_prt.sort()
+        for file in train_ffiles_prt:
+            with open(file, 'rb') as f:
+                train_port += pickle.load(f)
+
+        train_data = [f"{train}{prt}" for train, prt in zip(train_data, train_port)]
+    
+        test_ffiles_prt = glob.glob(os.path.join(folder, 'test_protflag_*'))
+        test_ffiles_prt.sort()
+        for file in test_ffiles_prt:
+            with open(file, 'rb') as f:
+                test_port += pickle.load(f)
+
+        test_data = [f"{test}{prt}" for test, prt in zip(test_data, test_port)]
+
     train_multi_dict, train_single_dict, train_label, attack_quantization_multi_set, attack_quantization_single_set\
           = make_quantization_dict(train_data, train_key)
 
@@ -161,7 +183,7 @@ def main(dataset_path, attack, change_feature, seperate, change_src, test_method
 
     # evaluate
     print("평가 시작")
-    file_name = f"cs({change_src})-cf({change_feature})-sepIP({separate_attackIP})-min({min_data})-n({n_components})-atk({attack})-conf({confidence})-sep({seperate})-test({test_method}).csv"
+    file_name = f"cs({change_src})-cf({change_feature})-prot({count_prot})-sepIP({separate_attackIP})-min({min_data})-n({n_components})-atk({attack})-conf({confidence})-sep({seperate})-test({test_method}).csv"
     save_file = f"./result/{dataset_path}/{file_name}.csv"
     evaluate(train_multi_dict, train_single_dict, train_label, attack_quantization_multi_set, attack_quantization_single_set,\
              test_multi_dict, test_single_dict, test_label, save_file)
@@ -172,21 +194,31 @@ if __name__ == "__main__":
     try:
         for data in ['CTU-Rbot','CTU-Neris']:
             try:
-                for attack in [0]: # 0이 정상 1이 공격 2가 혼합
+                for attack in [0, 1]: # 0이 정상 1이 공격 2가 혼합
                     try:
                         for change_feature in [False]:
                             try:
-                                for seperate in [False]:
+                                for count_prot in [True, False]:
                                     try:
-                                        for seperate_attackIP in [True]:
+                                        for seperate in [False]:
                                             try:
-                                                for change_src in [True]:
+                                                for seperate_attackIP in [True]:
                                                     try:
-                                                        for test_method in [True]: # true가 membership check
+                                                        for change_src in [True]:
                                                             try:
-                                                                for confidence in [1.28]:
-                                                                    main(data, attack, change_feature, seperate, change_src, test_method, confidence, seperate_attackIP)
-                                                            
+                                                                for test_method in [True]: # true가 membership check
+                                                                    try:
+                                                                        for confidence in [1.28]:
+                                                                            main(data, attack, change_feature, seperate, change_src, test_method, confidence, seperate_attackIP, count_prot)
+
+                                                                    except:
+                                                                        error_info = traceback.format_exc()
+                                                                        with open('log.txt', 'a') as f:
+                                                                            f.write(f"{data}-{attack} attack-{change_feature} changefeature-{seperate} sep-{change_src} change_src-{test_method} sep_attackIP-{seperate_attackIP} test에서 에러 발생\n")
+                                                                            now = datetime.now()
+                                                                            f.write(f"time: {now}\n")
+                                                                            f.write(f"{error_info}\n\n")
+                                                                        continue
                                                             except:
                                                                 error_info = traceback.format_exc()
                                                                 with open('log.txt', 'a') as f:
