c9b044e834e8142aa8981d8dee9a509a319d0396
youna1008 <xcv904@gmail.com>
2024-02-10 19:36:02 +0900

    add: choice N

--- log_quan.py
+++ log_quan.py
@@ -24,57 +24,96 @@ class log_Pattering:
         
         min_value = min(min_frequency_elements) # 최소 빈도값을 가진 원소들 중에서 최솟값 찾기
         return min_value
+    def make_countlist(self,data,ranges):
+        counts = [0] * (len(ranges) + 1)
+        
+        # 이진 탐색으로 각 데이터를 적절한 범위에 할당하여 카운트
+        for value in data:
+            # 이진 탐색으로 적절한 범위 찾기
+            left, right = 0, len(ranges)
+            while left < right:
+                mid = (left + right) // 2
+                if value < ranges[mid]:
+                        right = mid
+                else:
+                    left = mid + 1
+            # 적절한 범위에 카운트 추가
+            counts[left] += 1
+        
+        return counts
+    def entropy_f(self,count_list,total_count):
+        probabilities = [count / total_count for count in count_list]
+        entropy = -sum(p * np.log2(p) for p in probabilities if p != 0)
+        
+        n = len(count_list)
+        return entropy/np.log2(n)
     
     def make_boundary(self,logN_,feature_list):
-        logN = logN_
+        logN_n= logN_
         
         #min_data = min(feature_list)
         max_data = max(feature_list)
         #make original_list
-        original_list = [0]
-
-        tmp=1
-        i=1
-        while(tmp<=max_data):
-            tmp = logN**i
-            i+=1
-            original_list.append(tmp)
-        len_ = len(original_list)
         
-        #make real_boundary
-        real_boundary = [-1,0]
-        for idx,scale in enumerate(original_list):
+        Nlog_list = [1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9,2.0]
+        
+        min_entropy = 999999
+        before_entropy = 0 
+        
+        data_len = len(feature_list)
+        
+        real_boundary = [(2)**(i) for i in range(1,1000)]
+        
+        for logN in Nlog_list:
+            original_list = [0]
+            tmp=1
+            i=1
             
-            left = scale
-                            
-            if idx==(len_-1):
-                right = max_data+1    
-            else:
-                right = original_list[idx+1]
+            while(tmp<=max_data):
+                tmp = logN**i
+                i+=1
+                original_list.append(tmp)
             
-            filtered_array = feature_list[(feature_list > left) & (feature_list <= right)]
+            len_ = len(original_list)
+            
+            #make real_boundary
+            
+            tmp_boundary = [-1,0]
+            for idx,scale in enumerate(original_list):
+                
+                left = scale
+                                
+                if idx==(len_-1):
+                    right = max_data+1    
+                else:
+                    right = original_list[idx+1]
+                
+                filtered_array = feature_list[(feature_list > left) & (feature_list <= right)]
 
-            if len(filtered_array)==0:
-                if left !=0:
-                    real_boundary.append(right)
-                continue
-            
-            #filtered_int = np.round(filtered_array,1)
+                if len(filtered_array)==0:
+                    if left !=0:
+                        tmp_boundary.append(right)
+                    continue
+                midian_value = self.find_min_frequency_value(filtered_array)
 
-            #unique_elements, counts = np.unique(filtered_int, return_counts=True)            
-            #min_freq_bin = unique_elements[np.argmin(counts)]
+                tmp_boundary.append(midian_value)
+                
+            count_list = self.make_countlist(feature_list,tmp_boundary)
+            entropy = self.entropy_f(count_list,data_len)
             
-            #filtered_array = feature_list[(feature_list >= min_freq_bin-0.05) & (feature_list < (min_freq_bin+0.05))]            
-            midian_value = self.find_min_frequency_value(filtered_array)
-
-            # if midian_value == 0:
-            #     continue
-            real_boundary.append(midian_value)
+            if min_entropy >= entropy:
+                real_boundary = tmp_boundary
+                min_entropy = entropy
+                self.n_log = logN
+            elif before_entropy < entropy:
+                break
+            before_entropy = entropy
+        
         return real_boundary
     
     def multi_fit(self, data):
         boundary_dict = dict()
-        #data가 split_arrays임
+        
         array_ = np.array(data)
         arrayT = array_.T
         for idx,feature_data in tqdm(enumerate(arrayT),total = len(arrayT)):
