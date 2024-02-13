2d8f8a53810359b0dd36c746a17c2d6f3885826f
youna1008 <xcv904@gmail.com>
2024-02-13 16:53:33 +0900

    modify: min entropy2

--- log_quan.py
+++ log_quan.py
@@ -57,7 +57,7 @@ class log_Pattering:
         
         Nlog_list = [1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9,2.0]
         
-        min_entropy = 999999 
+        min_entropy = 9999999
         
         data_len = len(feature_list)
         
